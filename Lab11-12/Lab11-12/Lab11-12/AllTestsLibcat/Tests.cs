a98dfa1ad81b59c6c8f8231402d3669c0cb1212e
B1ackGhost <smirnovvlad313@gmail.com>
2023-12-09 18:44:07 +0300

    Added Lab11-12

---
+++ Lab11-12/Lab11-12/Lab11-12/AllTestsLibcat/Tests.cs
@@ -0,0 +1,107 @@
+﻿using TESTPOLAST.pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TESTPOLAST.Steps;
+using TESTPOLAST.Utils;
+using SeleniumExtras.PageObjects;
+
+namespace TESTPOLAST.AllTestsLibcat
+{
+    public class Tests
+    {
+        private Steps.Steps steps = new Steps.Steps();
+
+        private const string BookName = "Пенелопа Уорд";
+        private const string keyToReview = "Крутая книга";
+        private const string complaintText = "Жалоба!!!";
+
+
+        [SetUp]
+        public void Init()
+        {
+            steps.Init();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            steps.Cleanup();
+        }
+
+        [Test]
+        public void TestSearchBook()
+        {
+            steps.SearchBookMainPage(BookName);
+        }
+
+        [Test]
+        public void TestReviewBookSend()
+        {
+            steps.Login();
+            steps.OpenImperialVoyageBook();
+            steps.SendReviewKeys(keyToReview);
+
+        }
+
+        [Test]
+        public void TestAddFavoriteBook()
+        {
+            steps.Login();
+            steps.addFavoriteBookButtonVoyageBook();
+        }
+
+        [Test]
+        public void TestDeleteFavoriteBook()
+        {
+            steps.Login();
+            steps.deleteFavoriteBookButtonVoyageBook();
+        }
+
+        [Test]
+        public void TestBookMark()
+        {
+            steps.OpenImperialVoyageBook();
+            steps.bookMarkClick();
+        }
+
+        [Test]
+        public void TestIntervalChange()
+        {
+            steps.OpenImperialVoyageBook();
+            steps.intervalChange();
+        }
+
+        [Test]
+        public void TestAutorBooksOpen()
+        {
+            steps.OpenImperialVoyageBook();
+            steps.openAutorBooks();
+        }
+
+        [Test]
+        public void TestFantasticBooksOpen()
+        {
+            steps.OpenImperialVoyageBook();
+            steps.fantasticBooksOpen();
+        }
+
+        [Test]
+        public void TestSimilarBooksOpen()
+        {
+            steps.OpenImperialVoyageBook();
+            steps.similarBooksOpen();
+
+        }
+
+        [Test]
+        public void TestBadReviewSend()
+        {
+            steps.Login();
+            steps.OpenImperialVoyageBook();
+            steps.badReviewBook();
+        }
+    }
+}
---
+++ Lab11-12/Lab11-12/Lab11-12/Driver/DriverSingleton.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+
+namespace TESTPOLAST.Driver
+{
+	public class DriverSingleton
+    {
+        private static WebDriver driver;
+        private DriverSingleton() {}
+        public static WebDriver getDriver()
+        {
+            if (null == driver)
+            {
+				driver = new EdgeDriver();
+				driver.Manage().Timeouts().ImplicitWait.Add(TimeSpan.FromSeconds(30));
+				driver.Manage().Window.Maximize();
+			}
+            return driver;
+        }
+        public static void CloseDriver()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
